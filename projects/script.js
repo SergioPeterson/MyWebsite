d844cdd10b55c455fdd48e80fad797dfb2cba435
SergioPeterson <Sergiopeter2016@berkeley.edu>
2021-11-12 13:05:38 -0800

    Other Projects and Other Page

--- projects/script.js
+++ projects/script.js
@@ -1,3 +1,9 @@
 document.getElementById("curr_button").addEventListener("click", function(){
     window.location.replace("./projects-cur/index.html");
 })
+document.getElementById("fav_button").addEventListener("click", function(){
+    window.location.replace("./projects-fav/index.html");
+})
+document.getElementById("old_button").addEventListener("click", function(){
+    window.location.replace("./projects-old/index.html");
+})
